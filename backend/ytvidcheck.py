b5ae691aa72290c59ab377879acf9a4f3fb26360
Aditya JK <adityajk07@gmail.com>
2025-05-09 09:52:54 +0530

    Update ytvidcheck.py

--- backend/ytvidcheck.py
+++ backend/ytvidcheck.py
@@ -1,6 +1,6 @@
 from googleapiclient.discovery import build
 
-YOUTUBE_API_KEY = "AIzaSyCDL9D894_uMOUG3pe9ae4inLwpRMCf_BsRE"
+YOUTUBE_API_KEY = "[Enter API Key]"
 youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
 request = youtube.search().list(q="Python programming", part="snippet", type="video", maxResults=3)
 response = request.execute()
